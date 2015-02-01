e5bdd31d6e63c3c324a370fbc18c4fac3b8ff739
Claude Falguiere <cfalguiere@gmail.com>
2015-02-01 17:09:10 +0100

    configuration du cache

--- install-env.bash
+++ install-env.bash
@@ -13,6 +13,39 @@ sudo locale-gen en_US.UTF-8
 sudo locale-gen fr_FR.UTF-8
 
 echo '######################'
+echo 'Installing Proxy Cache'
+echo '######################'
+
+sudo apt-get install -y squid
+if [ ! -f /etc/squid3/squid.conf.original ]; then
+  # avoid rewriting when provision is run again
+  sudo cp /etc/squid3/squid.conf
+  sudo chmod a-w /etc/squid3/squid.conf.original
+  sudo echo http_port 8888 >> /etc/squid3/squid.conf
+  sudo sed -i '$a http_port 8888' /etc/squid3/squid.conf
+
+  sudo sed -i '$a http_proxy="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a https_proxy="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a ftp_proxy="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"' /etc/environment
+  sudo sed -i '$a HTTP_PROXY="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a HTTPS_PROXY="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a FTP_PROXY="http://localhost:8888/"' /etc/environment
+  sudo sed -i '$a NO_PROXY="localhost,127.0.0.1,localaddress,.localdomain.com"' /etc/environment
+
+  sudo cat >/tmp/95proxies <<'EOT'
+Acquire::http::proxy "http://localhost:8888/";
+Acquire::ftp::proxy "ftp://localhost:8888/";
+Acquire::https::proxy "https://localhost:8888/";
+EOT
+  sudo mv /tmp/95proxies /etc/apt/apt.conf.d/95proxies
+  sudo apt-get update
+
+  sudo service squid3 reload
+fi
+# sudo cat /var/log/squid3/access.log
+
+echo '######################'
 echo 'Installing node packages'
 echo '######################'
 
@@ -57,6 +90,7 @@ echo '######################'
 
 sudo apt-get install -y docker.io
 sudo docker run --rm -v /usr/local/bin:/target jpetazzo/nsenter
+
 
 echo '######################'
 
